873b9e7f4db610a2b7a5799f500c6fa9af93183c
Nathan <nathan.fenimore@gmail.com>
2018-08-29 08:00:57 +1000

    update challenge 03

--- 02_loops_arrays.rb
+++ 02_loops_arrays.rb
@@ -2,7 +2,7 @@
 # - create a complete sentence  that lets us know how you got to class and how long it took
 # - print this complete sentence
 
-#commute_time = 35
+#commute_time_mins = 35
 #puts "I walked to class today. It took #{commute_time} minutes to get to class."
 
 # you are working at a bar where you have a current backlog of drinks to make:
@@ -10,40 +10,39 @@
 # 2 waters
 # and
 # 6 beers
-cocktails = 3
-waters = 2
-beers = 6
+# cocktails = 3
+# waters = 2
+# beers = 6
+
+# drinks_no_arr = [3, 2, 6]
+drink_hash = {cocktails: {number: 3, price: 22, cost: 8}, waters: {number: 2, price: 6, cost: 0.15}, beers: {number: 6, price: 12, cost: 3} }
 
 # write a program that asks the customer for their order.
 # if they order a cocktail, add one to the number of cocktails you need to make,
 # if they order a water, add one to the number of waters you need to make,
 # if they order a beer, add one to the number of beers you need to pour
 
-
-
-begin 
 valid_drink = false
 while !valid_drink
     puts "What drink would you like (cocktail, water, beer)?"
     drink = gets.chomp
  case drink
  when 'cocktail'
-    cocktails += 1
+    drink_hash[:cocktails][:number] += 1
     valid_drink = true
  when 'water'
-    waters += 1
+    drink_hash[:waters][:number] += 1
     valid_drink = true
  when 'beer'
-    beers += 1
+    drink_hash[:beers][:number] += 1
     valid_drink = true
  else
     puts "Please select a drink from the list"
     valid_drink = false
  end
-end
 
 # print the final drinks order so you know what to make
-puts "I need to make #{cocktails} cocktails, #{waters} waters, and #{beers} beers."
+puts "I need to make #{drink_hash[:cocktails][:number]} cocktails, #{drink_hash[:waters][:number]} waters, and #{drink_hash[:beers][:number]} beers."
 end
 
 # ****
@@ -52,10 +51,19 @@ end
 # water sell for $6, and cost $0.15 to make
 
 # print out the total profit for the orders you have
-cocktail_profit = 22 - 8
-beer_profit = 12 - 3
-water_profit = 6 - 0.15
+# cocktail_profit = 22 - 8
+# beer_profit = 12 - 3
+# water_profit = 6 - 0.15
 
-total_profit = "%.2f" % (cocktails * cocktail_profit + beers * beer_profit + waters * water_profit)
+# drinks_price_arr = [22, 6, 12]
+# drinks_cost_arr = [8, 0.15, 3]
 
-puts "The total profit from sold drinks is $#{total_profit}."
+#total_profit = "%.2f" % (cocktails * cocktail_profit + beers * beer_profit + waters * water_profit)
+total_profit = 0
+drink_hash.each do |key, value|
+    total_profit += value[:number] * (value[:price] - value[:cost])    
+end
+
+puts "The total profit from sold drinks is $#{"%.2f" % total_profit}."
+
+#try using hashes
--- 03_packing_phones.rb
+++ 03_packing_phones.rb
@@ -18,5 +18,43 @@
 #
 #    Think about writing some pseudo code first that steps 
 #    through all the commands you need to write. 
-#
+
+def pack(phone_dim_array)
+    packing_volume = 1
+    phone_dimensions = phone_dim_array
+    box_dimensions = [32, 43, 22.1]
+    for index in 0...box_dimensions.length
+        packing_volume *= (box_dimensions[index] / phone_dimensions[index]).to_i
+    end
+    return packing_volume
+end
+
+
+optimal = 
+[pack([5, 7.4, 4]),
+pack([5, 4, 7.4]),
+pack([4, 5, 7.4]),
+pack([4, 7.4, 5]),
+pack([7.4, 5, 4]),
+pack([7.4, 4, 5])]
+
+puts "The most phones that can be packed into a box is #{optimal.max}."
+
 #    Keep it as simple as you can.
+
+# try this using the .permutations method on a single array
+
+# Matt's solution
+
+# phone = [5, 7.4, 4]
+# box = [32, 43, 22.1]
+
+# max_phones = 0
+# phone.permutation do |orient|
+#     print orient
+#     puts
+#     num_phones = (box[0] // orient[0]).to_i * (box[1] // orient[1]).to_i * (box[2] / orient[2]).to_i
+#     max_phones = num_phones if num_phones > max_phones
+# end
+
+# puts max_phones
